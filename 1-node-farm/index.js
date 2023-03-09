6bc93225845f7ec0a2a129cc2451331ab29b6113
garretgvc <christiansongg@gmail.com>
2023-03-09 15:09:11 -0600

    finished node farm 1 project

--- 1-node-farm/index.js
+++ 1-node-farm/index.js
@@ -1,8 +1,10 @@
 
+const { log } = require('console');
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-
+const slugify = require('slugify');
+const replaceTemplate = require('./modules/replaceTemplate');
 //---------------FILES---------------------//
 
 //Blcoking, Syncronous way
@@ -36,19 +38,6 @@ const url = require('url');
 
 //---------------SERVER---------------------//
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output
-
-}
 
 // Syncronous, create the object at the beginning and create it only once.
 const templOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -58,36 +47,43 @@ const templProduct = fs.readFileSync(`${__dirname}/templates/template-product.ht
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObject = JSON.parse(data);
 
+const slugs = dataObject.map(element => slugify(element.productName, {lower: true}));
+
 const server = http.createServer((request, response) => {
-    const pathName = request.url;
+    const { query, pathname } = url.parse(request.url, true);
+
 
     //Overview page
-    if (pathName === '/' || pathName === '/overview') {
+    if (pathname === '/' || pathname === '/overview') {
         response.writeHead(200, { 'Content-type': 'text/html' });
 
         const cardsHtml = dataObject.map(element => replaceTemplate(templCard, element)).join('');
         const output = templOverview.replace('{%PRODUCT_CARD%}', cardsHtml)
         response.end(output);
 
-    //Product page
-    } else if (pathName === '/product') {
-        response.end('This is the product');
+        //Product page
+    } else if (pathname === '/product') {
+        response.writeHead(200, { 'Content-type': 'text/html' });
+        const product = dataObject[query.id];
+        const output = replaceTemplate(templProduct, product);
+        response.end(output);
 
-    //API
-    } else if (pathName === '/api') {
+
+        //API
+    } else if (pathname === '/api') {
         // Read the data from the top level code
         response.writeHead(200, { 'Content-type': 'application/json' });
         // console.log(productData);
         response.end(data)
 
-    //Not Found
+        //Not Found
     } else {
-    response.writeHead(404, {
-        'Content-type': 'text/html',
-        'my-own-header': 'Hello-world'
-    });
-    response.end('<h1>Page not found!</h1>');
-}
+        response.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'Hello-world'
+        });
+        response.end('<h1>Page not found!</h1>');
+    }
     // response.end('Hello from the sever!');
 });
 
